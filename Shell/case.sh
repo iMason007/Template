dcb0020e32167ed3988e0003dd2af1bb9316998c
Mason <635761952@qq.com>
2018-02-27 00:48:12 +0800

    add context in

---
+++ Shell/case.sh
@@ -0,0 +1,29 @@
+#!/usr/sbin/bash
+
+case $1 in
+
+    xxx)
+    
+        if []; then
+        else
+        fi
+        exit $?
+        
+        ;;
+        
+    xxx)
+    
+        if []; then
+        else
+        fi
+        exit $?
+        
+        ;;
+    
+    *)
+    
+        exit 10
+        
+        ;;
+
+esac
